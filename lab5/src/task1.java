d35d3ee01d5ac30e7833126d9ddd68fe66f0837b
Ashrateppey Q <nurshidinnarimanov@gmail.com>
2026-02-19 13:46:56 +0500

    Add files via upload

---
+++ lab5/src/task1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число N: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ lab5/src/task5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class task5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+        int sum = 0;
+
+        do {
+            System.out.print("Введите число (0 для выхода): ");
+            number = scanner.nextInt();
+            sum += number;
+        } while (number != 0);
+
+        System.out.println("Сумма введённых чисел равна: " + sum);
+    }
+}
